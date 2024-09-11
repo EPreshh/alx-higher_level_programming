3f143dd2dca7b1181f9fae7140889849db6147f6
Precious_o <preciousadebola@gmail.com>
2024-09-11 16:44:25 -0700

    Update function to use not more than 2 print statements

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
+    result = []
     for i in str:
         if ord(i) >= 97 and ord(i) <= 122:
-            print("{}".format(chr(ord(i) - 32)), end="")
+            result.append(chr(ord(i) - 32))
         else:
-            print("{}".format(i), end="")
-    print()
+            result.append(i)
+    print("{}".format("".join(result)))
