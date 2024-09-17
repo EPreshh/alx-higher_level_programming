9ff4e95fa52eb3437ad5e1a2fe5c97689b86a2e7
Precious_o <preciousadebola@gmail.com>
2024-09-16 21:45:47 -0700

    Initialise max_element outside loop

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,8 +2,8 @@
 def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
+    max_element = my_list[0]
     for i in my_list:
-        max_element = my_list[i]
         if i > max_element:
             max_element = i
-        return max_element
+    return max_element
