87949b4aaddff74d8919b49e1c09ceae37498830
Precious_o <preciousadebola@gmail.com>
2024-09-16 21:10:33 -0700

    Update function

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         if len(row) > 0:
             for i in range(len(row)):
-                if i < len(row) -1:
+                if i < len(row) - 1:
                     print("{:d}".format(row[i]), end=" ")
                 else:
                     print("{:d}".format(row[i]), end="$\n")
