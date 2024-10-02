6be47e12c7dd9cda01e15d8d486e621b0f89003c
Precious_o <preciousadebola@gmail.com>
2024-10-02 16:03:41 -0700

    Add a function that divides 2 integers and prints the result.

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,8 @@
+def safe_print_division(a, b):
+    try:
+        z = a / b
+    except ZeroDivisionError:
+        z = None
+    finally:
+        print("Inside result: {}".format(z))
+    return z
