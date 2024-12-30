abfff21848e9fad50fceaaed8656bdb2bb54841f
Precious_o <preciousadebola@gmail.com>
2024-12-30 11:27:25 -0800

    Add script that lists all rows of a table from the database

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,5 @@
+-- lists all rows od tge table 'first_table'
+-- from the database 'hbtn_0c_0' in my MYSQL server
+-- database name will be passsed as an argument
+-- of the mysql command
+SELECT * FROM `first_table`
