290d968207fb99a3e07af0cdc33358abe79eaf3b
Precious_o <preciousadebola@gmail.com>
2024-12-30 12:03:04 -0800

    Add script that displays number of records in a table

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,5 @@
+-- displays the number of records with a specific id in 'first_table'
+-- database name will be passed as an argument of the mysql command
+SELECT COUNT(*)
+  FROM `first_table`
+  WHERE `id` = 89;
