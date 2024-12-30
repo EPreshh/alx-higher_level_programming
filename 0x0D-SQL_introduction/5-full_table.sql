ef61e70ec575a197f4e96e50fc93e33a3a91e89c
Precious_o <preciousadebola@gmail.com>
2024-12-30 10:53:59 -0800

    Add script that prints the full description - create table description of a table

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,5 @@
+-- prints the full description - create table statement
+-- of the table 'first_table
+-- from the database 'hbtn_0c_0' without using 
+-- the DESCRIBE or EXPLAIN statements
+SHOW CREATE TABLE hbtn_0c_0.first_table;
