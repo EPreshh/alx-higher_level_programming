711d2763bc509a56671fa3ff3280a8872417d2e3
Precious_o <preciousadebola@gmail.com>
2024-12-30 13:32:01 -0800

    Add script that lists all records of a table omitting rows without a required value

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,8 @@
+-- lists all records of the table 'second_table'
+-- rows without a name value should be ommitted
+-- results display score and name
+-- records listed by descending score
+-- database name will be passed as an argument to the mysql command
+SELECT `score`, `name`
+  FROM `second_table`
+  WHERE `score` IS NOT NULL AND `name` IS NOT NULL;
